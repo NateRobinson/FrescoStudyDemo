2e6aaaa8cc0ffa84d6c91b2317a5fb4dbfa26bcf
NateRobinson <15951084423@163.com>
2015-07-23 17:11:55 +0800

    添加Fresco初始化

--- app/src/main/java/com/nate/frescostudydemo/MyApplication.java
+++ app/src/main/java/com/nate/frescostudydemo/MyApplication.java
@@ -2,6 +2,8 @@ package com.nate.frescostudydemo;
 
 import android.app.Application;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 /**
  * Created by Nate on 2015/7/22.
  */
@@ -9,6 +11,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        Fresco.initialize(this);//初始化Fresco
     }
 }
