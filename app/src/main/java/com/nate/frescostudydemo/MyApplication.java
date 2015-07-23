9f2910969836c9a7ef8e33f395825c88726960f0
NateRobinson <15951084423@163.com>
2015-07-23 16:58:53 +0800

    Initial commit

---
+++ app/src/main/java/com/nate/frescostudydemo/MyApplication.java
@@ -0,0 +1,14 @@
+package com.nate.frescostudydemo;
+
+import android.app.Application;
+
+/**
+ * Created by Nate on 2015/7/22.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
