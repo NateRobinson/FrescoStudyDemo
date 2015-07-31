3df365aae0bf9d9825dc5c979990ae62fa0b8182
840501291@qq.com <840501291@qq.com>
2015-07-31 14:20:02 +0800

    本地图片复用

--- app/src/main/java/com/nate/frescostudydemo/activity/MultiAndMultiPlexImgActivity.java
+++ app/src/main/java/com/nate/frescostudydemo/activity/MultiAndMultiPlexImgActivity.java
@@ -26,6 +26,8 @@ public class MultiAndMultiPlexImgActivity extends BaseActivity {
     Button multiImgBtn;
     @Bind(R.id.thumbnailImgBtn)
     Button thumbnailImgBtn;
+    @Bind(R.id.multiplexImgBtn)
+    Button multiplexImgBtn;
 
     @Override
     public void initContentLayout() {
@@ -36,6 +38,7 @@ public class MultiAndMultiPlexImgActivity extends BaseActivity {
     public void initListener() {
         multiImgBtn.setOnClickListener(this);
         thumbnailImgBtn.setOnClickListener(this);
+        multiplexImgBtn.setOnClickListener(this);
     }
 
     @Override
@@ -47,8 +50,8 @@ public class MultiAndMultiPlexImgActivity extends BaseActivity {
     public void bindClick(int viewId) {
         if (viewId == R.id.multiImgBtn) {
             //同一张图片，不同品质的两个uri
-            Uri lowResUri =Uri.parse("http://img1.gamedog.cn/2012/03/11/19-120311133617-50.jpg");
-            Uri highResUri=Uri.parse("http://img5.duitang.com/uploads/item/201312/03/20131203153823_Y4y8F.jpeg");
+            Uri lowResUri = Uri.parse("http://img1.gamedog.cn/2012/03/11/19-120311133617-50.jpg");
+            Uri highResUri = Uri.parse("http://img5.duitang.com/uploads/item/201312/03/20131203153823_Y4y8F.jpeg");
             DraweeController controller = Fresco.newDraweeControllerBuilder()
                     .setLowResImageRequest(ImageRequest.fromUri(lowResUri))
                     .setImageRequest(ImageRequest.fromUri(highResUri))
@@ -57,12 +60,27 @@ public class MultiAndMultiPlexImgActivity extends BaseActivity {
             myImageView.setController(controller);
         } else if (viewId == R.id.thumbnailImgBtn) {
             //将本地图片地址转换成Uri
-            Uri uri=Uri.fromFile(new File("/storage/sdcard0/DCIM/Camera/1434088743922.jpg"));
+            Uri uri = Uri.fromFile(new File("/storage/sdcard0/DCIM/Camera/1434088743922.jpg"));
             ImageRequest request = ImageRequestBuilder.newBuilderWithSource(uri)
                     .setLocalThumbnailPreviewsEnabled(true)
                     .build();
             DraweeController controller = Fresco.newDraweeControllerBuilder()
                     .setImageRequest(request)
+                    .setOldController(myImageView.getController())
+                    .build();
+            myImageView.setController(controller);
+        } else if (viewId == R.id.multiplexImgBtn) {
+            //本地图片的复用
+            //在请求之前，还会去内存中请求一次图片，没有才会先去本地，最后去网络uri
+            //本地准备复用图片的uri  如果本地这个图片不存在，会自动去加载下一个uri
+            Uri uri1 = Uri.fromFile(new File("/storage/sdcard0/DCIM/Camera/1434088722.jpg"));
+            //图片的网络uri
+            Uri uri2 = Uri.parse("http://img5.duitang.com/uploads/item/201312/03/20131203153823_Y4y8F.jpeg");
+            ImageRequest request1 = ImageRequest.fromUri(uri1);
+            ImageRequest request2 = ImageRequest.fromUri(uri2);
+            ImageRequest[] requests = {request1, request2};
+            DraweeController controller = Fresco.newDraweeControllerBuilder()
+                    .setFirstAvailableImageRequests(requests)
                     .setOldController(myImageView.getController())
                     .build();
             myImageView.setController(controller);
